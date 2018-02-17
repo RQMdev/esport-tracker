8e27e0d51b8b8b0228cb79d84e33fabecc181a36
Benoit Laurent <benoit.jacques.felix@gmail.com>
2018-02-17 17:49:32 +0100

    add app.js

--- app.js
+++ app.js
@@ -1,10 +1,24 @@
 const express = require('express')
+const morgan = require('morgan')
+const bodyParser = require('body-parser')
+const expressJwt = require('express-jwt')
+const mongoose = require('mongoose')
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/esport-tracker')
+
 const app = express()
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
+// Middlewares
+app.use(morgan('dev'))
+app.use(bodyParser.json())
 
-app.listen(3000, function(){
-  console.log('App listening on port 3000')
-})
+// Routes
+app.use('/users', require('./routes/users'))
+app.use('/events', require('./routes/events'))
+app.use('/places', require('./routes/places'))
+
+// Start Server
+const port = process.env.PORT || 3001
+app.listen(port)
+console.log(`Server listening at ${port}`)
